bc81167d14314a4b33192a5ad35d2d4270d8824f
임채민 <limchaemin0070@gmail.com>
2025-11-28 11:36:39 +0900

    feat : 로그인 관련 InvalidCredentialsError 추가

--- backend/src/errors/AuthError.ts
+++ backend/src/errors/AuthError.ts
@@ -1,3 +1,13 @@
 import { CustomError } from "./CustomErrors";
 
 // 인증, 인가와 관련된 에러
+
+export class InvalidCredentialsError extends CustomError {
+  constructor() {
+    super(
+      401,
+      "INVALID_CREDENTIALS",
+      "이메일 또는 비밀번호가 올바르지 않습니다."
+    );
+  }
+}
