95acdf712622a3b5190c75e1e83b9a154bf79ca9
임채민 <limchaemin0070@gmail.com>
2025-12-02 13:39:04 +0900

    style : 회원가입 페이지 기본 레이아웃 퍼블리싱 (#50)

--- frontend/src/pages/User/RegisterPage.tsx
+++ frontend/src/pages/User/RegisterPage.tsx
@@ -140,158 +140,176 @@ export const RegisterPage = () => {
     };
 
     return (
-        <div className="flex flex-col items-center gap-5 w-full h-full my-[50px] px-5">
-            <h2 className="text-2xl text-center text-sample-blue font-bold py-3 ">
-                회원가입 페이지
-            </h2>
-            <form
-                className="flex flex-col gap-2 w-full"
-                onSubmit={handleSubmit}
-                noValidate
-            >
-                <input
-                    type="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    onBlur={(e) => {
-                        const result = validateEmail(e.target.value);
-                        if (!result.isValid && result.error) {
-                            setErrors((prev) => ({
-                                ...prev,
-                                email: result.error || '',
-                            }));
-                        } else {
-                            setErrors((prev) => {
-                                const newErrors = { ...prev };
-                                delete newErrors.email;
-                                return newErrors;
-                            });
-                        }
-                    }}
-                    placeholder="이메일을 입력하세요"
-                    className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-sample-blue"
-                />
-                <button
-                    type="button"
-                    onClick={handleEmailVerification}
-                    className="btn-primary-filled"
-                    disabled={isEmailVerified}
+        <div className="flex flex-row items-center w-full h-full min-[]:">
+            {/* 이미지 컨테이너 */}
+            <div className="flex items-center justify-center h-full bg-gradient-primary w-[60%] " />
+            {/* 폼 데이터 영역 */}
+            <div className="flex flex-col items-center justify-center gap-5 h-full flex-1 px-12">
+                <h2 className="text-2xl text-sample-blue font-bold py-3 w-full">
+                    회원가입
+                </h2>
+                <form
+                    className="flex flex-col w-full gap-2"
+                    onSubmit={handleSubmit}
+                    noValidate
                 >
-                    인증번호 전송
-                </button>
-                {isVerificationCodeSent && !isEmailVerified && (
-                    <>
+                    <div className="flex flex-row gap-2">
                         <input
-                            type="text"
-                            value={verificationCode}
-                            onChange={(e) =>
-                                setVerificationCode(e.target.value)
-                            }
+                            type="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
                             onBlur={(e) => {
-                                const result = validateCode(e.target.value);
+                                const result = validateEmail(e.target.value);
                                 if (!result.isValid && result.error) {
                                     setErrors((prev) => ({
                                         ...prev,
-                                        code: result.error || '',
+                                        email: result.error || '',
                                     }));
                                 } else {
                                     setErrors((prev) => {
                                         const newErrors = { ...prev };
-                                        delete newErrors.code;
+                                        delete newErrors.email;
                                         return newErrors;
                                     });
                                 }
                             }}
-                            placeholder="인증번호를 입력하세요"
-                            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-sample-blue"
+                            placeholder="이메일을 입력하세요"
+                            className={
+                                errors.email ? 'input-error' : 'input-base'
+                            }
                         />
                         <button
                             type="button"
-                            onClick={handleVerifyCode}
-                            className="btn-primary-filled"
+                            onClick={handleEmailVerification}
+                            className="btn-primary-filled w-[30%]"
+                            disabled={isEmailVerified}
                         >
-                            인증번호 확인
+                            인증
                         </button>
-                    </>
-                )}
-                {isEmailVerified && (
-                    <div className="text-green-600 text-sm">
-                        이메일 인증이 완료되었습니다.
                     </div>
-                )}
-                <input
-                    type="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    onBlur={(e) => {
-                        const result = validatePassword(e.target.value);
-                        if (!result.isValid && result.error) {
-                            setErrors((prev) => ({
-                                ...prev,
-                                password: result.error || '',
-                            }));
-                        } else {
-                            setErrors((prev) => {
-                                const newErrors = { ...prev };
-                                delete newErrors.password;
-                                return newErrors;
-                            });
+
+                    {isVerificationCodeSent && !isEmailVerified && (
+                        <>
+                            <input
+                                type="text"
+                                value={verificationCode}
+                                onChange={(e) =>
+                                    setVerificationCode(e.target.value)
+                                }
+                                onBlur={(e) => {
+                                    const result = validateCode(e.target.value);
+                                    if (!result.isValid && result.error) {
+                                        setErrors((prev) => ({
+                                            ...prev,
+                                            code: result.error || '',
+                                        }));
+                                    } else {
+                                        setErrors((prev) => {
+                                            const newErrors = { ...prev };
+                                            delete newErrors.code;
+                                            return newErrors;
+                                        });
+                                    }
+                                }}
+                                placeholder="인증번호를 입력하세요"
+                                className="input-base"
+                            />
+                            <button
+                                type="button"
+                                onClick={handleVerifyCode}
+                                className="btn-primary-filled"
+                            >
+                                인증번호 확인
+                            </button>
+                        </>
+                    )}
+                    {isEmailVerified && (
+                        <div className="text-green-600 text-sm">
+                            이메일 인증이 완료되었습니다.
+                        </div>
+                    )}
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        onBlur={(e) => {
+                            const result = validatePassword(e.target.value);
+                            if (!result.isValid && result.error) {
+                                setErrors((prev) => ({
+                                    ...prev,
+                                    password: result.error || '',
+                                }));
+                            } else {
+                                setErrors((prev) => {
+                                    const newErrors = { ...prev };
+                                    delete newErrors.password;
+                                    return newErrors;
+                                });
+                            }
+                        }}
+                        placeholder="비밀번호를 입력하세요"
+                        className={
+                            errors.password ? 'input-error' : 'input-base'
                         }
-                    }}
-                    placeholder="비밀번호를 입력하세요"
-                    className="w-full px-4 py-2 border border-gray-800 rounded-md focus:outline-none focus:ring-2 focus:ring-sample-blue"
-                />
-                <input
-                    type="confirmPassword"
-                    value={confirmPassword}
-                    onChange={(e) => setConfirmPassword(e.target.value)}
-                    onBlur={(e) => {
-                        const result = validateConfirmPassword(
-                            password,
-                            e.target.value,
-                        );
-                        if (!result.isValid && result.error) {
-                            setErrors((prev) => ({
-                                ...prev,
-                                confirmPassword: result.error || '',
-                            }));
-                        } else {
-                            setErrors((prev) => {
-                                const newErrors = { ...prev };
-                                delete newErrors.confirmPassword;
-                                return newErrors;
-                            });
+                    />
+                    <input
+                        type="confirmPassword"
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                        onBlur={(e) => {
+                            const result = validateConfirmPassword(
+                                password,
+                                e.target.value,
+                            );
+                            if (!result.isValid && result.error) {
+                                setErrors((prev) => ({
+                                    ...prev,
+                                    confirmPassword: result.error || '',
+                                }));
+                            } else {
+                                setErrors((prev) => {
+                                    const newErrors = { ...prev };
+                                    delete newErrors.confirmPassword;
+                                    return newErrors;
+                                });
+                            }
+                        }}
+                        placeholder="비밀번호를 한번 더 입력하세요"
+                        className={
+                            errors.confirmPassword
+                                ? 'input-error'
+                                : 'input-base'
                         }
-                    }}
-                    placeholder="비밀번호를 한번 더 입력하세요"
-                    className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-sample-blue"
-                />
-                <input
-                    type="nickname"
-                    value={nickname}
-                    onChange={(e) => setNickname(e.target.value)}
-                    onBlur={(e) => {
-                        const result = validateNickname(e.target.value);
-                        if (!result.isValid && result.error) {
-                            setErrors((prev) => ({
-                                ...prev,
-                                nickname: result.error || '',
-                            }));
-                        } else {
-                            setErrors((prev) => {
-                                const newErrors = { ...prev };
-                                delete newErrors.nickname;
-                                return newErrors;
-                            });
+                    />
+                    <input
+                        type="nickname"
+                        value={nickname}
+                        onChange={(e) => setNickname(e.target.value)}
+                        onBlur={(e) => {
+                            const result = validateNickname(e.target.value);
+                            if (!result.isValid && result.error) {
+                                setErrors((prev) => ({
+                                    ...prev,
+                                    nickname: result.error || '',
+                                }));
+                            } else {
+                                setErrors((prev) => {
+                                    const newErrors = { ...prev };
+                                    delete newErrors.nickname;
+                                    return newErrors;
+                                });
+                            }
+                        }}
+                        placeholder="사용할 닉네임을 입력하세요"
+                        className={
+                            errors.nickname ? 'input-error' : 'input-base'
                         }
-                    }}
-                    placeholder="사용할 닉네임을 입력하세요"
-                    className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-sample-blue"
-                />
-                <button type="submit" className="btn-primary-filled">
-                    회원가입 신청 버튼
-                </button>
-            </form>
+                    />
+                    <button type="submit" className="btn-primary-filled">
+                        회원가입
+                    </button>
+                </form>
+            </div>
         </div>
     );
 };
