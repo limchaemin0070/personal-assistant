8ee0a980f4e49a47fc5a7e9fa3f7e24bcae9883c
임채민 <limchaemin0070@gmail.com>
2025-12-02 16:52:12 +0900

    feat : 메인 페이지에 컴포넌트 임포트

--- frontend/src/pages/main/MainPage.tsx
+++ frontend/src/pages/main/MainPage.tsx
@@ -1,3 +1,28 @@
+import { useState } from 'react';
+import { CalendarLayout } from '@/components/layout/MainLayout';
+import { CalendarSidebar } from '@/components/calendar/CalendarSidebar';
+import { CalendarMain } from '@/components/calendar/CalendarMain';
+import { RightSidebar } from '@/components/layout/RightSidebar';
+
 export const MainPage = () => {
-    return <div>메인 페이지 입니다.</div>;
+    const [isLeftSidebarOpen, setIsLeftSidebarOpen] = useState(true);
+    const [isRightSidebarOpen, setIsRightSidebarOpen] = useState(true);
+    return (
+        <CalendarLayout>
+            {isLeftSidebarOpen && (
+                <CalendarSidebar onClose={() => setIsLeftSidebarOpen(false)} />
+            )}
+            <CalendarMain
+                onToggleLeftSidebar={() =>
+                    setIsLeftSidebarOpen(!isLeftSidebarOpen)
+                }
+                onToggleRightSidebar={() =>
+                    setIsRightSidebarOpen(!isRightSidebarOpen)
+                }
+            />
+            {isRightSidebarOpen && (
+                <RightSidebar onClose={() => setIsRightSidebarOpen(false)} />
+            )}
+        </CalendarLayout>
+    );
 };
