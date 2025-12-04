505920a2bb41b779a637b63a5d6d839d29b771da
임채민 <limchaemin0070@gmail.com>
2025-12-04 15:06:35 +0900

    feat : 캘린더 날짜 출력

--- frontend/src/components/calendar/Calandar.tsx
+++ frontend/src/components/calendar/Calandar.tsx
@@ -1,35 +1,41 @@
 import React from 'react';
 import { CalendarWeek } from './CalendarWeek';
+import { useCalendar } from '@/hooks/calendar/useCalendar';
 
 // 전체 캘린더 뷰
 export const Calander: React.FC = () => {
-    // 구현중
-    const { weeks, currentDate } = useCalendar();
+    const {
+        currentDate,
+        view,
+
+        weekDays,
+        headerText,
+
+        goToNextMonth,
+        goToPrevMonth,
+    } = useCalendar();
 
     return (
         <div className="flex h-full w-full items-center justify-center bg-white">
             <div className="text-center text-gray-500">
                 {/* 캘린더 제목 */}
-                <h3>
-                    {headers.current.year} {headers.current.month + 1}
-                </h3>
+                <h3>{headerText}</h3>
                 {/* 캘린더 월 변경 컨트롤러 */}
                 <div>
-                    <button type="button" onClick={movePrevMonth}>
+                    <button type="button" onClick={goToPrevMonth}>
                         이전 달
                     </button>
-                    <button type="button" onClick={moveNextMonth}>
+                    <button type="button" onClick={goToNextMonth}>
                         다음 달
                     </button>
                 </div>
-                {/* 요일 레인*/}
-                <WeekdayHeaders />
-                {weeks.map((weekDays) => {
+                {/* 요일 레인 */}
+                {weekDays.map((week) => {
                     return (
                         <CalendarWeek
-                            weekDays={weekDays}
-                            weekLayout={weekLayout}
-                            maxVisibleEvents={4}
+                            weekDays={week}
+                            // weekLayout={weekLayout}
+                            // maxVisibleEvents={4}
                         />
                     );
                 })}
--- frontend/src/components/calendar/CalendarWeek.tsx
+++ frontend/src/components/calendar/CalendarWeek.tsx
@@ -1,12 +1,18 @@
+import type { CalendarDay } from '@/utils/calendar/CalendarUtils';
+
 interface CalendarWeekProps {
     weekDays: CalendarDay[];
-    weekLayout: Map<string, EventWithLane[]>;
-    maxVisibleEvents?: number;
+    // weekLayout: Map<string, EventWithLane[]>;
+    // maxVisibleEvents?: number;
 }
 
 // 캘린더의 한 주차 컴포넌트
 // 1 row
-export const CalendarWeek = () => {
+export const CalendarWeek = ({
+    weekDays,
+    // weekLayout,
+    // maxVisibleEvents,
+}: CalendarWeekProps) => {
     return (
         <div>
             {/* 주차별로 렌더링 */}
@@ -14,8 +20,8 @@ export const CalendarWeek = () => {
                 return (
                     <div>
                         {/* 일자별 렌더링 */}
-                        <div>날짜(숫자)</div>
-                        <div>이벤트 목록 렌더링(map 이용)</div>
+                        <div>{day.dayOfMonth}</div>
+                        {/* <div>{maxVisibleEvents}</div> */}
                     </div>
                 );
             })}
