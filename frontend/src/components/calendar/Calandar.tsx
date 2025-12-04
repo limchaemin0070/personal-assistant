5f06c2fde22d61d6e144285b58978897f3c21249
임채민 <limchaemin0070@gmail.com>
2025-12-04 16:41:22 +0900

    feat : 캘린더 뷰 추가

--- frontend/src/components/calendar/Calandar.tsx
+++ frontend/src/components/calendar/Calandar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { CalendarWeek } from './CalendarWeek';
 import { useCalendar } from '@/hooks/calendar/useCalendar';
+import { SlArrowLeft, SlArrowRight } from 'react-icons/sl';
 
 // 전체 캘린더 뷰
 export const Calander: React.FC = () => {
@@ -16,30 +17,38 @@ export const Calander: React.FC = () => {
     } = useCalendar();
 
     return (
-        <div className="flex h-full w-full items-center justify-center bg-white">
-            <div className="text-center text-gray-500">
+        <div className="flex flex-col flex-1 h-full w-full bg-white">
+            <div className="flex flex-row align-middle gap-5">
                 {/* 캘린더 제목 */}
-                <h3>{headerText}</h3>
+                <h3 className="text-2xl font-semibold">{headerText}</h3>
                 {/* 캘린더 월 변경 컨트롤러 */}
-                <div>
-                    <button type="button" onClick={goToPrevMonth}>
-                        이전 달
+                <div className="flex flex-row gap-2">
+                    <button
+                        className="p-2"
+                        type="button"
+                        onClick={goToPrevMonth}
+                    >
+                        <SlArrowLeft />
                     </button>
-                    <button type="button" onClick={goToNextMonth}>
-                        다음 달
+                    <button
+                        className="p-2"
+                        type="button"
+                        onClick={goToNextMonth}
+                    >
+                        <SlArrowRight />
                     </button>
                 </div>
-                {/* 요일 레인 */}
-                {weekDays.map((week) => {
-                    return (
-                        <CalendarWeek
-                            weekDays={week}
-                            // weekLayout={weekLayout}
-                            // maxVisibleEvents={4}
-                        />
-                    );
-                })}
             </div>
+            {/* 요일 레인 */}
+            {weekDays.map((week) => {
+                return (
+                    <CalendarWeek
+                        weekDays={week}
+                        // weekLayout={weekLayout}
+                        // maxVisibleEvents={4}
+                    />
+                );
+            })}
         </div>
     );
 };
--- frontend/src/components/calendar/CalendarWeek.tsx
+++ frontend/src/components/calendar/CalendarWeek.tsx
@@ -14,14 +14,29 @@ export const CalendarWeek = ({
     // maxVisibleEvents,
 }: CalendarWeekProps) => {
     return (
-        <div>
+        <div className="calendar-week-container">
             {/* 주차별로 렌더링 */}
-            {weekDays.map((day) => {
+            {weekDays.map((day, index) => {
+                const isWeekend = index === 0 || index === 6; // 일요일(0) 또는 토요일(6)
                 return (
-                    <div>
+                    <div
+                        className={`calendar-day-cell ${
+                            isWeekend ? 'calendar-day-cell-weekend' : ''
+                        }`}
+                    >
                         {/* 일자별 렌더링 */}
-                        <div>{day.dayOfMonth}</div>
-                        {/* <div>{maxVisibleEvents}</div> */}
+                        <div
+                            className={`calendar-day-number ${
+                                !day.isCurrentMonth
+                                    ? 'calendar-day-number-other-month'
+                                    : ''
+                            }`}
+                        >
+                            {day.dayOfMonth}
+                        </div>
+                        <div className="calendar-events-grid">
+                            {/* 이후 여기에 이벤트들을 최대 4개까지 렌더링 */}
+                        </div>
                     </div>
                 );
             })}
