fb7502067ec49414d3066ebb173208d0a078ca8b
임채민 <limchaemin0070@gmail.com>
2025-12-21 03:01:51 +0900

    chore : 사용하지 않는 가져오기 제거

--- frontend/src/hooks/alarm/useToggleAlarmActive.ts
+++ frontend/src/hooks/alarm/useToggleAlarmActive.ts
@@ -1,6 +1,5 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { alarmService } from '@/services/alarm.service';
-import { useToastStore } from '../useToastStore';
 import { queryKeys } from '@/lib/queryKeys';
 import { useMutationErrorHandler } from '../useMutationErrorHandler';
 
--- frontend/src/hooks/reminder/useToggleReminderComplete.ts
+++ frontend/src/hooks/reminder/useToggleReminderComplete.ts
@@ -1,6 +1,5 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { reminderService } from '@/services/reminder.service';
-import { useToastStore } from '../useToastStore';
 import { queryKeys } from '@/lib/queryKeys';
 import { useMutationErrorHandler } from '../useMutationErrorHandler';
 
